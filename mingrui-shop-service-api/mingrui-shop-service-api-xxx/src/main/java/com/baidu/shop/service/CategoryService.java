9e150b99ee30c654395e266855b27eeedb7cc955
JiaHangh <jia435262644@163.com>
2021-01-20 17:53:39 +0800

    项目

--- mingrui-shop-service-api/mingrui-shop-service-api-xxx/src/main/java/com/baidu/shop/service/CategoryService.java
+++ mingrui-shop-service-api/mingrui-shop-service-api-xxx/src/main/java/com/baidu/shop/service/CategoryService.java
@@ -2,8 +2,10 @@ package com.baidu.shop.service;
 
 import com.baidu.shop.com.baidu.shop.base.Result;
 import com.baidu.shop.entity.CategoryEntity;
+import com.google.gson.JsonObject;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
@@ -13,4 +15,8 @@ public interface CategoryService {
     @ApiOperation(value = "通过父id查询商品分类信息")
     @GetMapping(value = "category/list")
     Result<List<CategoryEntity>> getCategoryByPid(Integer pid);
+
+    @ApiOperation(value = "通过id删除分类")
+    @DeleteMapping("/category/delete")
+    Result<JsonObject> delCategory(Integer id);
 }
--- mingrui-shop-service/mingrui-shop-service-xxx/src/main/java/com/baidu/shop/service/impl/CategoryServiceImpl.java
+++ mingrui-shop-service/mingrui-shop-service-xxx/src/main/java/com/baidu/shop/service/impl/CategoryServiceImpl.java
@@ -5,7 +5,10 @@ import com.baidu.shop.com.baidu.shop.base.Result;
 import com.baidu.shop.entity.CategoryEntity;
 import com.baidu.shop.mapper.CategoryMapper;
 import com.baidu.shop.service.CategoryService;
+import com.baidu.shop.utils.ObjectUtil;
+import com.google.gson.JsonObject;
 import org.springframework.web.bind.annotation.RestController;
+import tk.mybatis.mapper.entity.Example;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -23,6 +26,7 @@ public class CategoryServiceImpl extends BaseApiService implements CategoryServi
     @Resource
     private CategoryMapper categoryMapper;
 
+    //查询
     @Override
     public Result<List<CategoryEntity>> getCategoryByPid(Integer pid) {
         CategoryEntity categoryEntity = new CategoryEntity();
@@ -30,5 +34,30 @@ public class CategoryServiceImpl extends BaseApiService implements CategoryServi
         List<CategoryEntity> list = categoryMapper.select(categoryEntity);
 
         return this.setResultSuccess(list);
+    }
+
+    //删除
+    @Override
+    public Result<JsonObject> delCategory(Integer id) {
+        if (ObjectUtil.isNull(id)) return this.setResultError("id不存在");
+        CategoryEntity categoryEntity = categoryMapper.selectByPrimaryKey(id);
+        if (ObjectUtil.isNull(categoryEntity)) return this.setResultError("没有此数据");
+        if (categoryEntity.getIsParent()==1) return this.setResultError("当前节点为父节点");
+
+        Example example = new Example(CategoryEntity.class);
+        example.createCriteria().andEqualTo("parentId",categoryEntity.getParentId());
+        List<CategoryEntity> list = categoryMapper.selectByExample(example);
+        //如果查出父节点下的节点<=1时，删除了这个节点，就把该节点的父节点变为叶子节点
+        if (list.size()<=1){
+            CategoryEntity parentCategory = new CategoryEntity();
+            parentCategory.setId(categoryEntity.getParentId());
+            parentCategory.setParentId(0);
+            categoryMapper.updateByPrimaryKeySelective(parentCategory);
+        }
+
+        //如果查出父节点下有多个节点，直接删除
+        categoryMapper.deleteByPrimaryKey(id);
+
+        return this.setResultSuccess();
     }
 }
